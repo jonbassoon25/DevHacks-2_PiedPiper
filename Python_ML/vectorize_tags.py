908aad38d32ac4c3bfab6365df64a62a71e8b7af
Jonathan Hanson <jonbassoon25@gmail.com>
2025-09-20 16:40:23 -0700

    Fix tags loading

--- Python_ML/vectorize_tags.py
+++ Python_ML/vectorize_tags.py
@@ -1,8 +1,11 @@
 import numpy as np
 
-df = np.load("location_tags.npy", allow_pickle=True)
+df = np.array(np.load("./Python_ML/location_tags.npy", allow_pickle=True))
 
-tags = df.flatten()
-tags = np.array(tags)
+
+tags = set()
+for tags_ in df:
+	for tag_ in tags_:
+		tags.add(tag_)
 
 print(tags)
