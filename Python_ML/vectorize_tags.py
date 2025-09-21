0fcaa362cdd022c01bcce0e32220f9b5779365a8
Jonathan Hanson <jonbassoon25@gmail.com>
2025-09-20 17:57:16 -0700

    Add vectorization calculations

--- Python_ML/vectorize_tags.py
+++ Python_ML/vectorize_tags.py
@@ -1,7 +1,8 @@
 import numpy as np
+import csv, math
 
+# Import tags
 df = np.array(np.load("./Python_ML/location_tags.npy", allow_pickle=True))
-
 
 tags = set()
 for tags_ in df:
@@ -10,14 +11,29 @@ for tags_ in df:
 
 tags = np.array(list(tags), dtype=str)
 
-vectors = np.zeros(shape=(df.shape[0], tags.shape[0]), dtype=np.int8)
+# Vectorize tags
+vectors = np.zeros(shape=(df.shape[0], tags.shape[0] + 1), dtype=np.uint8)
 for i in range(vectors.shape[0]):
-	for j in range(vectors.shape[1]):
+	for j in range(vectors.shape[1] - 1):
 		if tags[j] in df[i]:
 			vectors[i][j] = 1
 		else:
 			vectors[i][j] = 0
 
-np.save("./Python_ML/location_tag_vectors.npy", vectors, True)
+# Add ratings from 0 - 255 ( rating * ( 255 / 5)  * ((1/(-rating/10)-1) + 1) )
+with open("./location_database.csv") as csvFile:
+	count = 0
+	for line in csv.reader(csvFile):
+		if (count == 0):
+			count += 1
+			continue
+		rating = float(line[2])
+		reviews = int(line[3])
+		vectors[count - 1][vectors.shape[1] - 1] = math.trunc(rating * 51 * (( 1 / (-(reviews / 10) - 1) ) + 1))
+		
+		count += 1
+
+
+np.save("./Python_ML/location_vectors.npy", vectors, True)
 
 print("\n".join(map(str, list(vectors))))
